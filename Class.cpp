8f356b27173771b419144b2c1c209a44eef4dded
stanikeva <72407345+stanikeva@users.noreply.github.com>
2021-04-03 12:38:09 +0300

    Add files via upload
    whatever

---
+++ Class.cpp
@@ -0,0 +1,19 @@
+#include "Triangle.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//
+/*jfkjcb
+*/
+
+Triangle::Triangle()
+{
+    xa = 0;
+    ya = 0;
+    xb = 0;
+    yb = 0;
+    xc = 0;
+    yc = 0;
+}
